a3dad572d6c6b5c2518499d3a5b2effca2fd81cc
Challelign Tilahun <chalie2123@gmail.com>
2023-12-26 10:21:12 +0300

    adding advert to product list by slice each product list

--- src/components/ProductFeed.jsx
+++ src/components/ProductFeed.jsx
@@ -4,6 +4,8 @@ import Product from "./Product";
 
 const ProductFeed = () => {
 	const [posts, setPosts] = useState([]);
+	const copiedPosts = [...posts];
+
 	useEffect(() => {
 		const fetchPost = async () => {
 			const response = await fetch("https://fakestoreapi.com/products");
@@ -14,18 +16,55 @@ const ProductFeed = () => {
 		fetchPost();
 	}, []);
 	return (
-		<div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+		<div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
 			{/* <h1>Products Here ..</h1> */}
-			{posts?.map(({ id, title, price, description, category, image }) => (
-				<Product
-					id={id}
-					title={title}
-					price={price}
-					description={description}
-					category={category}
-					image={image}
-				/>
-			))}
+
+			{posts
+				?.slice(0, 4)
+				.map(({ id, title, price, description, category, image }) => (
+					<Product
+						id={id}
+						title={title}
+						price={price}
+						description={description}
+						category={category}
+						image={image}
+					/>
+				))}
+
+			<img
+				className="md:col-span-full"
+				src="/image/amazonbg1.jpg"
+				loading="lazy"
+				height={100}
+			/>
+			<div className="md:col-span-2 ">
+				{posts
+					?.slice(4, 5)
+					.map(({ id, title, price, description, category, image }) => (
+						<Product
+							id={id}
+							title={title}
+							price={price}
+							description={description}
+							category={category}
+							image={image}
+						/>
+					))}
+			</div>
+
+			{posts
+				?.slice(5, copiedPosts.length)
+				.map(({ id, title, price, description, category, image }) => (
+					<Product
+						id={id}
+						title={title}
+						price={price}
+						description={description}
+						category={category}
+						image={image}
+					/>
+				))}
 		</div>
 	);
 };
